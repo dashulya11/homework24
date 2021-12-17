a2edb34b9c3ece7e343cbca277899690aa30b7e1
dashulya11 <91964833+dashulya11@users.noreply.github.com>
2021-12-17 17:01:22 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Сделайте функцию, каждый вызов который будет генерировать
+// случайные числа от 1 до 100, но так, чтобы они не повторялись,
+//     пока не будут перебраны все числа из этого промежутка.
+//     Решите задачу через замыкания - в замыкании должен хранится
+//     массив чисел, которые уже были сгенерированы функцией.
+
+const getRandomNumber = () => Math.floor(Math.random() * 100) +1;
+
+function generateId() {
+    const newArray = [];
+
+    function generateNumber() {
+        const randomNum = getRandomNumber();
+
+        if (newArray.length === 100) return newArray;
+        
+        if (newArray.includes(randomNum)) {
+            return generateNumber();
+        } 
+
+        newArray.push(randomNum);
+        return newArray;
+       
+        
+    }
+
+    return generateNumber;
+}
+
+const idGenerator = generateId();
+let foo;
+
+for (let i = 0; i <= 100; i++){
+    foo = idGenerator();
+}
+
+console.log(foo);
+
+// foo = foo.sort((a, b) => a - b);
+    
+
+
+
